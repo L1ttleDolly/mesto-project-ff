419c1e1d19001b93d1c43d92cedc7f01a0958520
Vitaliya <vitaliya_malyutina@mail.ru>
2025-04-22 19:56:20 +0300

    first push

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+const container = document.querySelector('.content') // ограничиваю область поиска
+const addButton = container.querySelector('.profile__add-button') // кнопка добавления
+const cardContainer = document.querySelector('.places__list') // контененр карточек
+const cardTemplate = document.querySelector('#card-template').content
 
-// @todo: DOM узлы
+function cardAdd (cardItem) { // функция добавления карточек на страницу
+  initialCards.forEach(function(card) { // обращаюсь к массиву через forEach , запускаю цикл перебора объектов массива, card - объект массива
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true) // клонирую карточку в переменную
+    const deleteButton = cardElement.querySelector('.card__delete-button') //нахожу кнопку в карточке
+    deleteButton.addEventListener('click',function() {
+      cardDelete(cardElement)
+      })
+    Object.assign(cardElement.querySelector('.card__image'),{ // через Object.assign (в первый аргумент пиши куда хочу добавить, {пишу что хочу добавить, указывая объект массива и его значение})
+      src: card.link,
+      alt: card.name,
+    })
+    cardElement.querySelector('.card__title').textContent = card.name // нахожу тайтл и через текст контент перезаписываю тайтл из объекта массива
+    cardContainer.append(cardElement) // через аppend вставляю карточку(кардЭлемент) в контейнер(кардКонтейнер(который плейс лист))
+  })
+}
 
-// @todo: Функция создания карточки
+cardAdd(initialCards) // вызываю  функцию передавая в аргументы массив данных
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function cardDelete(cardElement){ //вызыываю функцию удаленеия карточки в аргументы передаю карточку
+cardElement.remove()
+}
