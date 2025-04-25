4c0584944dbbe1ad31ca05ff8c11d43d77d66b27
Vitaliya <vitaliya_malyutina@mail.ru>
2025-04-25 16:01:13 +0300

    описание строк

--- scripts/index.js
+++ scripts/index.js
@@ -4,33 +4,33 @@ const addButton = container.querySelector(".profile__add-button"); // кнопк
 const cardContainer = $(".places__list"); // контененр карточек
 const cardTemplate = $("#card-template").content;
 
-function createCard(card, deleteCard) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+function createCard(card, deleteCard) { //функиця добавления карты
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); //клонирую
   const deleteButton = cardElement.querySelector(".card__delete-button"); //нахожу кнопку в карточке
 
-  Object.assign(cardElement.querySelector(".card__image"), {
+  Object.assign(cardElement.querySelector(".card__image"), { //чеоез этот метод (target, {source1, source2,})
     src: card.link,
     alt: card.name,
   });
 
-  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__title").textContent = card.name; //title
 
-  deleteButton.addEventListener("click", function () {
+  deleteButton.addEventListener("click", function () { //обработчик по клику внутри функции с карточкойй, чтобы было понятно где происходит клик
     deleteCard(cardElement); // deletecard - функция
   });
 
-  return cardElement;
+  return cardElement; //возвращаю 1 заполненную карточку
 }
 
-function placeCards() {
+function placeCards() {// через эту функцию заполняю карточки
   initialCards.forEach(function (card) {
     const cardData = createCard(card, deleteCard);
     cardContainer.append(cardData);
   });
 
-  function deleteCard(cardElement) {
+  function deleteCard(cardElement) { //удаление карточки
     cardElement.remove();
   }
 }
 
-placeCards();
+placeCards(); //вызываб функцию массива
